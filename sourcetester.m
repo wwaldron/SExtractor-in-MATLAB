fe32e69e648380aa89157b52036cd4c908e50412
Will Waldron <wvw0330@uah.edu>
2016-10-27 14:31:43 -0500

    Changed sourcetester to work with laptop and corrected the filewrite function by transposing arrays.

--- sourcetester.m
+++ sourcetester.m
@@ -17,27 +17,27 @@ clc
 % jb9g01020_drc_slice.fits =  ACS,  475 nm
 
 % The FITS Files of interest (full path recommended)
-fitsFiles = {'/home/wwaldron/Documents/DoctoralResearch-Dissertation/Images/09-01-16_MAST_DATA/sliced_images/ib9ga1010_drc_slice.fits',...
-'/home/wwaldron/Documents/DoctoralResearch-Dissertation/Images/09-01-16_MAST_DATA/sliced_images/iblwa1010_drz_slice.fits',...
-'/home/wwaldron/Documents/DoctoralResearch-Dissertation/Images/09-01-16_MAST_DATA/sliced_images/jb9g01010_drc_slice.fits',...
-'/home/wwaldron/Documents/DoctoralResearch-Dissertation/Images/09-01-16_MAST_DATA/sliced_images/jb9g01020_drc_slice.fits'};
+fitsFiles = {'/home/wwaldron/Pictures/tmp/sliced_images/ib9ga1010_drc_slice.fits',...
+'/home/wwaldron/Pictures/tmp/sliced_images/iblwa1010_drz_slice.fits',...
+'/home/wwaldron/Pictures/tmp/sliced_images/jb9g01010_drc_slice.fits',...
+'/home/wwaldron/Pictures/tmp/sliced_images/jb9g01020_drc_slice.fits'};
 
 % Helper files
-confFile  = '/home/wwaldron/Documents/DoctoralResearch-Dissertation/Images/09-01-16_MAST_DATA/ESO_137-001_SExtractor_Files/mingSExtractor2.conf';
-paramFile = '/home/wwaldron/Documents/DoctoralResearch-Dissertation/Images/09-01-16_MAST_DATA/ESO_137-001_SExtractor_Files/mingSExtractor.param';
-convFile  = '/home/wwaldron/Software/SExtractor/ConvolutionKernals/default.conv';
-nnFile    = '/home/wwaldron/Software/SExtractor/ConvolutionKernals/default.nnw';
+confFile  = '/home/wwaldron/Pictures/tmp/SExtractorFiles/eso_137-001.conf';
+paramFile = '/home/wwaldron/Pictures/tmp/SExtractorFiles/eso_137-001.param';
+convFile  = '/usr/share/sextractor/default.conv';
+nnFile    = '/usr/share/sextractor/default.nnw';
 
 % Name-Value Pair Arguments
 nvPairs.SExtractorCommand = 'sextractor';
 nvPairs.AnalysisThreshold = 10;
 nvPairs.DetectThreshold   = [6,6,6,6];
-nvPairs.MaxAngSepTrueSrc  = 0.0;
+nvPairs.MaxAngSepTrueSrc  = 0.2;
 nvPairs.Gain              = [1.54,2.5,2.0,2.0];
 nvPairs.SeeingFWHM        = [0.4 ,0.1,0.5,0.5];
 nvPairs.DetectMinArea     = 4;
 nvPairs.PixelScale        = 0;
-nvPairs.RegOutputFormat   = 'XYWorld';
+nvPairs.RegOutputFormat   = 'SAOImage';
 
 data = finduncommonsources(fitsFiles,confFile,paramFile,convFile,nnFile,...
     nvPairs);
